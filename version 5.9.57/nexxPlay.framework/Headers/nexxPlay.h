2f6bfe7614434528a0a016ea09d110eea2e79c7d
Daniel Leberle <leberle.daniel@gmail.com>
2018-02-15 11:33:52 +0100

    v5.9.57

---
+++ version 5.9.57/nexxPlay.framework/Headers/nexxPlay.h
@@ -0,0 +1,20 @@
+//
+//  nexxPlay.h
+//  nexxPlay
+//
+//  Created by Michael Kugler on 21/05/15.
+//  Copyright (c) 2015 nexx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <nexxPlay/ObjectiveCExceptions.h>
+
+//! Project version number for nexxPlay.
+FOUNDATION_EXPORT double nexxPlayVersionNumber;
+
+//! Project version string for nexxPlay.
+FOUNDATION_EXPORT const unsigned char nexxPlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nexxPlay/PublicHeader.h>
+
+
