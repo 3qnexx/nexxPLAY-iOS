c4750e784e5d30626c95d0ccd963d51b9e1a8f01
Daniel Leberle <leberle.daniel@gmail.com>
2018-09-18 17:04:24 +0200

    script to strip the framework for published apps updated

--- strip-framework.sh
+++ strip-framework.sh
@@ -1,23 +1,23 @@
 FRAMEWORKS_DIRECTORY="${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}"
-NEXXPLAY_FRAMEWORKS=`find $FRAMEWORKS_DIRECTORY -type d -name "nexxPlay.framework"`
+NEXXPLAY_FRAMEWORKS=`find $FRAMEWORKS_DIRECTORY -type d -name "nexxPlay.framework" -o -name "DailymeSdk.framework" -o -name "GoogleInteractiveMediaAds.framework"`
 
 for framework in $NEXXPLAY_FRAMEWORKS; do
-    framework_name=`basename $framework`
-    framework_binary_path=$framework"/${framework_name%.*}"
+framework_name=`basename $framework`
+framework_binary_path=$framework"/${framework_name%.*}"
 
-    archs="$(lipo -info "${framework_binary_path}" | rev | cut -d ':' -f1 | rev)"
-    stripped=""
-    for arch in $archs; do
-        if ! [[ "${VALID_ARCHS}" == *"$arch"* ]]; then
-            lipo -remove "$arch" -output "$framework_binary_path" "$framework_binary_path" || exit 1
-            stripped="$stripped $arch"
-        fi
-    done
-    
-    if [[ "$stripped" != "" ]]; then
-        echo "Stripped $framework_binary_path of architectures:$stripped"
-        if [ "${CODE_SIGNING_REQUIRED}" == "YES" ]; then
-        /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements "${framework_binary_path}"
-        fi
-    fi
+archs="$(lipo -info "${framework_binary_path}" | rev | cut -d ':' -f1 | rev)"
+stripped=""
+for arch in $archs; do
+if ! [[ "${VALID_ARCHS}" == *"$arch"* ]]; then
+lipo -remove "$arch" -output "$framework_binary_path" "$framework_binary_path" || exit 1
+stripped="$stripped $arch"
+fi
+done
+
+if [[ "$stripped" != "" ]]; then
+echo "Stripped $framework_binary_path of architectures:$stripped"
+if [ "${CODE_SIGNING_REQUIRED}" == "YES" ]; then
+/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements "${framework_binary_path}"
+fi
+fi
 done
